fb8459c6db4f848e22c8b658b7d420f951612b24
Eric Chauvin <162734842+EricsSourceCode@users.noreply.github.com>
2024-09-27 13:05:57 -0600

    Add files via upload

---
+++ NeuralNet1.cs
@@ -0,0 +1,261 @@
+// Copyright Eric Chauvin 2024.
+
+
+
+// This is licensed under the GNU General
+// Public License (GPL).  It is the
+// same license that Linux has.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
+
+
+using System;
+
+
+
+// namespace
+
+
+
+// There is that old saying:
+// Make it work first, then make it work fast.
+
+
+
+public class NeuralNet1
+{
+private MainData mData;
+private NeuronLayer1 inputLayer;
+private NeuronLayer1 hiddenLayer;
+private NeuronLayer1 outputLayer;
+private VectorFlt errorOutVec;
+private VectorArray inputMatrix;
+private VectorArray labelMatrix;
+
+
+
+
+private NeuralNet1()
+{
+}
+
+
+
+internal NeuralNet1( MainData useMainData,
+                  VectorArray useMatrix,
+                  VectorArray useLabelMatrix )
+{
+mData = useMainData;
+inputMatrix = useMatrix;
+labelMatrix = useLabelMatrix;
+
+int last = inputMatrix.getLastAppend();
+if( labelMatrix.getLastAppend() != last )
+  {
+  throw new Exception(
+          "labelMatrix doesn't match last." );
+  }
+
+inputLayer = new NeuronLayer1( mData );
+hiddenLayer = new NeuronLayer1( mData );
+outputLayer = new NeuronLayer1( mData );
+errorOutVec = new VectorFlt( mData );
+}
+
+
+
+internal void test()
+{
+mData.showStatus( "NeuralNet1.test()." );
+
+setupNetTopology();
+
+float randMax = 1.0F / inputLayer.getSize();
+mData.showStatus( "randMax: " + randMax );
+
+setRandomWeights( randMax );
+
+for( int row = 0; row < 1; row++ )
+  {
+  setInputRow( row );
+  forwardPass( row );
+  backprop();
+  }
+
+mData.showStatus( "NeuralNet.test() end." );
+}
+
+
+
+private void setupNetTopology()
+{
+int col = inputMatrix.getColumns();
+
+// Plus 1 for the bias at zero.
+int layerSize = col + 1;
+
+inputLayer.setSize( layerSize );
+
+// The weights aren't used here.
+inputLayer.setWeightArSize( 1 );
+
+hiddenLayer.setSize( layerSize );
+hiddenLayer.setWeightArSize( layerSize );
+
+// One for the bias at zero, and two more.
+outputLayer.setSize( 3 );
+
+outputLayer.setWeightArSize( layerSize );
+
+errorOutVec.setSize( 3 );
+}
+
+
+
+
+private void setRandomWeights( float maxWeight )
+{
+// The input layer doesn't use weights.
+// inputLayer.setRandomWeights()
+
+hiddenLayer.setRandomWeights( maxWeight );
+outputLayer.setRandomWeights( maxWeight );
+}
+
+
+
+
+private void setInputRow( int row )
+{
+// Set the input layer neurons (activation
+// value) from data matrix.
+
+int col = inputMatrix.getColumns();
+
+// Plus 1 for the bias at zero.
+int layerSize = col + 1;
+mData.showStatus( "InputLayer size: " +
+                   layerSize );
+
+if( layerSize != inputLayer.getSize())
+  {
+  throw new Exception(
+             "layerSize != inputLayer size." );
+  }
+
+// The bias that isn't used in this row.
+inputLayer.setActivationAt( 0, 1.0F );
+
+for( int count = 0; count < col; count++ )
+  {
+  float val = inputMatrix.getVal( row, count );
+  inputLayer.setActivationAt( count + 1, val );
+  }
+}
+
+
+
+/*
+To match up with the errorVec?
+Or what?
+======
+private void setLabelRow( int row )
+{
+int col = labelMatrix.getColumns();
+
+// Plus 1 for the bias at zero.
+int layerSize = col + 1;
+mData.showStatus( "LabelMatrix layerSize: " +
+                   layerSize );
+
+// The bias that isn't used in this row.
+inputLayer.setActivationAt( 0, 1.0F );
+
+for( int count = 0; count < col; count++ )
+  {
+  float val = inputMatrix.getVal( row, count );
+  inputLayer.setActivationAt( count + 1, val );
+  }
+}
+*/
+
+
+
+private void forwardPass( int row )
+{
+mData.showStatus( " " );
+mData.showStatus( "forwardPass(): " + row );
+
+hiddenLayer.calcZ( inputLayer );
+hiddenLayer.calcActReLU();
+
+outputLayer.calcZ( hiddenLayer );
+outputLayer.calcActSigmoid();
+
+// The value at zero is the bias.
+float aOut1 = outputLayer.getActivationAt( 1 );
+float aOut2 = outputLayer.getActivationAt( 2 );
+
+mData.showStatus( "aOut1: " + aOut1 );
+mData.showStatus( "aOut2: " + aOut2 );
+
+float label1 = labelMatrix.getVal( row, 1 );
+float label2 = labelMatrix.getVal( row, 2 );
+
+mData.showStatus( "label1: " + label1 );
+mData.showStatus( "label2: " + label2 );
+
+// If the output value is less than the label
+// value then it is positive.  Meaning the
+// weights have to be adjusted up.
+// If the output value is more than the
+// label value then it is negative, so
+// the weights have to be adjusted down.
+// (Adding a negative number.)
+
+float error1 = label1 - aOut1;
+float error2 = label2 - aOut2;
+
+// mData.showStatus( "error1: " + error1 );
+// mData.showStatus( "error2: " + error2 );
+
+errorOutVec.setVal( 1, error1 );
+errorOutVec.setVal( 2, error2 );
+}
+
+
+
+private void backprop()
+{
+// Adjust weight for bias at index zero.
+
+// At each step you calculate the gradient
+// and make a small step.  Bigger error
+// means a bigger step.
+// error = d - y
+// d is desired output.  y is calc output.
+
+
+mData.showStatus( " " );
+mData.showStatus( "backprop(): " );
+
+// y - y(hat)
+float error1 = errorOutVec.getVal( 1 );
+float error2 = errorOutVec.getVal( 2 );
+mData.showStatus( "error1: " + error1 );
+mData.showStatus( "error2: " + error2 );
+
+
+// ==== Cost function.
+
+
+// outputLayer.setDeltaAt( 1, error1 );
+// outputLayer.setDeltaAt( 2, error2 );
+
+
+mData.showStatus( "End of backprop()." );
+}
+
+
+
+} // Class
---
+++ NeuronLayer1.cs
@@ -0,0 +1,199 @@
+// Copyright Eric Chauvin 2024.
+
+
+
+// This is licensed under the GNU General
+// Public License (GPL).  It is the
+// same license that Linux has.
+// https://www.gnu.org/licenses/gpl-3.0.html
+
+
+
+using System;
+
+
+
+// namespace
+
+
+
+public class NeuronLayer1
+{
+private MainData mData;
+private Neuron1[] neuronAr;
+
+
+
+private NeuronLayer1()
+{
+}
+
+
+
+
+internal NeuronLayer1( MainData useMainData )
+{
+mData = useMainData;
+
+try
+{
+neuronAr = new Neuron1[2];
+for( int count = 0; count < 2; count++ )
+  neuronAr[count] = new Neuron1( mData );
+
+}
+catch( Exception ) // Except )
+  {
+  // freeAll();
+  throw new Exception(
+          "Not enough memory for NeuronLayer." );
+  }
+}
+
+
+
+internal int getSize()
+{
+return neuronAr.Length;
+}
+
+
+
+internal void setSize( int howBig )
+{
+int arraySize = neuronAr.Length;
+if( howBig == arraySize )
+  return;
+
+try
+{
+neuronAr = new Neuron1[howBig];
+for( int count = 0; count < howBig; count++ )
+  neuronAr[count] = new Neuron1( mData );
+
+}
+catch( Exception ) // Except )
+  {
+  throw new Exception(
+          "Not enough memory for NeuronLayer." );
+  }
+}
+
+
+
+
+internal void setWeightArSize( int setTo )
+{
+// All of the neurons in this layer have
+// the same number of inputs from the
+// previous layer.
+
+int max = neuronAr.Length;
+for( int count = 0; count < max; count++ )
+  neuronAr[count].setWeightVecSize( setTo );
+
+}
+
+
+
+internal float getActivationAt( int where )
+{
+// By convention, index zero is for the
+// bias, so this should always return 1 for
+// the Activation at index 0.
+
+// Is that true of every layer?
+
+if( where == 0 )
+  return 1;
+
+int max = neuronAr.Length;
+RangeT.test( where, 0, max - 1,
+       "NeuronLayer.getActivationAt() range." );
+
+return neuronAr[where].getActivation();
+}
+
+
+
+internal void setActivationAt( int where,
+                               float setTo )
+{
+// By convention, it is the bias.
+if( where == 0 )
+  {
+  neuronAr[where].setActivation( 1.0F );
+  return;
+  }
+
+int max = neuronAr.Length;
+RangeT.test( where, 0, max - 1,
+       "NeuronLayer.setActivationAt() range." );
+
+neuronAr[where].setActivation( setTo );
+}
+
+
+
+internal void setRandomWeights( float maxWeight )
+{
+int max = neuronAr.Length;
+for( int count = 0; count < max; count++ )
+  {
+  neuronAr[count].setRandomWeights(
+                    maxWeight, count );
+  }
+}
+
+
+
+internal void calcZ( NeuronLayer1 prevLayer )
+{
+int max = neuronAr.Length;
+for( int count = 0; count < max; count++ )
+  {
+  neuronAr[count].calcZ( prevLayer );
+  }
+}
+
+
+
+internal void calcActSigmoid()
+{
+int max = neuronAr.Length;
+for( int count = 0; count < max; count++ )
+  {
+  neuronAr[count].calcActSigmoid();
+  }
+}
+
+
+
+internal void calcActReLU()
+{
+int max = neuronAr.Length;
+for( int count = 0; count < max; count++ )
+  {
+  neuronAr[count].calcActReLU();
+  }
+}
+
+
+
+internal void setDeltaAt( int where,
+                          float setTo )
+{
+// By convention, it is the bias.
+// if( where == 0 )
+
+int max = neuronAr.Length;
+RangeT.test( where, 1, max - 1,
+       "NeuronLayer.setDeltaAt() range." );
+
+neuronAr[where].setDelta( setTo );
+}
+
+
+
+
+} // Class
